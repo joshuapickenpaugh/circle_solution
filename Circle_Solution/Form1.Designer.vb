e46cafcb61498001291ccaa79e6b6399fab913e8
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-03 09:40:57 -0500

    Commit

--- Circle_Solution/Form1.Designer.vb
+++ Circle_Solution/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmMain
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,70 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.txtDiameter = New System.Windows.Forms.TextBox()
+        Me.lblCircumference = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(108, 99)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(150, 150)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'txtDiameter
+        '
+        Me.txtDiameter.Location = New System.Drawing.Point(35, 50)
+        Me.txtDiameter.Name = "txtDiameter"
+        Me.txtDiameter.Size = New System.Drawing.Size(100, 20)
+        Me.txtDiameter.TabIndex = 1
+        '
+        'lblCircumference
+        '
+        Me.lblCircumference.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblCircumference.Location = New System.Drawing.Point(158, 47)
+        Me.lblCircumference.Name = "lblCircumference"
+        Me.lblCircumference.Size = New System.Drawing.Size(100, 23)
+        Me.lblCircumference.TabIndex = 2
+        Me.lblCircumference.Text = " "
+        Me.lblCircumference.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(34, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "&Diameter"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(373, 261)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblCircumference)
+        Me.Controls.Add(Me.txtDiameter)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Circle Circumference"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents txtDiameter As TextBox
+    Friend WithEvents lblCircumference As Label
+    Friend WithEvents Label1 As Label
 End Class
